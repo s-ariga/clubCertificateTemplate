0b7be3bec3b102a08c433e25dfcfddfc9a660d39
s-ariga <seiichi.ariga@gmail.com>
2019-07-01 04:04:48 +0900

    総合団体の英語名変更

---
+++ scripts/Positions.py
@@ -0,0 +1,32 @@
+﻿from enum import Enum, auto
+
+class Positions(Enum):
+    AR60 = 0
+    FR3X40 = 1
+    FR60PR = 2
+    AR60W = 3
+    R3X40 = 4
+    R60PR = 5
+    AR60PR = 7
+#    ARMIX = 6
+
+POSITIONS = ['AR60',
+             'FR3X40',
+             'FR60PR',
+             'AR60W',
+             'R3X40',
+             'R60PR',
+             'AR60PR',
+             'ARMIX']
+
+def position_exists(name):
+    '''
+    種目名の文字列が存在するか調べて返す
+    parameters 
+    name 種目名
+
+    returns
+    bool
+    '''
+    assert name in POSITIONS, 'Wrong position name: ' + name 
+    return True if name in POSITIONS else False
