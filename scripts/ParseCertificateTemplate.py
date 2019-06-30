4c3ad5d38593c955208dc2bf49be4da7216de2f1
s-ariga <seiichi.ariga@gmail.com>
2019-07-01 04:06:38 +0900

    必要ないファイルの消去

--- scripts/ParseCertificateTemplate.py
+++ scripts/ParseCertificateTemplate.py
@@ -5,26 +5,35 @@
 クラブ戦用
 賞状テンプレートを読んで、htmlとして返す
 '''
-
+import os
 import jinja2
 import pandas as pd
 import datetime
 
+import Positions as posi
+
+POSI = 'Position'
 SCORE = 'Score'
 RANK = 'Rank'
 TEAM = 'Team'
+NAME = 'Name'
+NAME1 = 'Name1'
+NAME2 = 'Name2'
+NAME3 = 'Name3'
 
+CERT_RANKS = 6
 
 class ParseCertificateTemplate():
-    def __init__(self, path, data):
-        self.tmplt_loader = jinja2.FileSystemLoader(searchpath="./")
+    def __init__(self, path, filename, data):
+        self.tmplt_loader = jinja2.FileSystemLoader(searchpath=path)
         self.tmplt_env = jinja2.Environment(loader=self.tmplt_loader)
-
-        self.tmplt_file = path
+        self.tmplt_file = filename
+        assert os.path.exists(path + filename), 'テンプレートファイルが存在しない'
         self.df = data
+        assert len(self.df) == CERT_RANKS, '表彰者数が6じゃない'
 
         today = datetime.date.today()
-        self.year = self.get_reiwa(today.year)
+        self.year = self.get_reiwa(today.year, gannen = True)
         self.month, self.day = today.month, today.day
 
     def getCertificate(self):
@@ -50,14 +59,17 @@ class ParseCertificateTemplate():
         当日以外の賞状を作るとき用
         年、月、日を設定
         '''
-        self.year = self.get_reiwa(year)
+        self.year = self.get_reiwa(year, gannen = True)
         self.month = month
         self.day = day
 
-    def get_reiwa(self, year):
+    def get_reiwa(self, year, gannen = False):
+        '''
+        西暦 -> 令和　の変換
+        '''
+        assert year > 2018, '令和以前の日時'
         reiwa = year - 2018
-        return "元" if reiwa == 1 else reiwa
-
+        return "元" if reiwa == 1 and gannen == True else reiwa
 
 if __name__ == '__main__':
     print("クラブ戦用賞状印刷")
--- scripts/createCerts.py
+++ scripts/createCerts.py
@@ -1,48 +1,65 @@
 ﻿#!/usr/bin/env python
 # -*- coding : utf-8 -*-
 
+import os
 import datetime
+import argparse
+
 import jinja2
-import pdfkit
-import ParseCertificateTemplate as tp
 import pandas as pd
+
+import ParseCertificateTemplate as tp
+import Positions as pos
 
 # テンプレート置き場
 TEMPLATE_DIR = "../template/"
-TEMPLATE_INDIVIDUAL = TEMPLATE_DIR + "certificate-indivi.html"
-TEMPLATE_MIXTEAM = TEMPLATE_DIR + "certificate-mixteam.html"
-TEMPLATE_TEAM_POSI = TEMPLATE_DIR + "certificate-tem-posi.html"
-TEMPLATE_TEAM_ALL = TEMPLATE_DIR + "certificate-team-all.html"
+TEMPLATE_INDIVIDUAL = "certificate-indivi.html"
+TEMPLATE_MIXTEAM = "certificate-mixteam.html"
+TEMPLATE_TEAM_POSI = "certificate-tem-posi.html"
+TEMPLATE_TEAM_COMBI = "certificate-team-combi.html"
 
 # 成績置き場
 RESULT_DIR = "../results/"
 RESULT_INDIVIDUAL = RESULT_DIR + "individual.xlsx"
 RESULT_MIXTEAM = RESULT_DIR + "mixteam.xlsx"
 RESULT_TEAM_POSI = RESULT_DIR + "team_posi.xlsx"
-RESULT_TEAM_ALL = RESULT_DIR + "team_all.xlsx"
+RESULT_TEAM_COMBI = RESULT_DIR + "team_combi.xlsx"
 
 # 出力先
 OUTPUT_DIR = "../output/"
-OUTPUT_TEAM_ALL = OUTPUT_DIR+"teamAll{0}.html"
+OUTPUT_TEAM_COMBI = OUTPUT_DIR+"teamCombined{0}.html"
+
 def createCerts():
-    
-    
     pass
 
-def createCertsTeamAll():
+def createCertsTeamCombined():
 
-    result = pd.read_excel(RESULT_TEAM_ALL)
+    result = pd.read_excel(RESULT_TEAM_COMBI)
     
-    html = tp.ParseCertificateTemplate('certificate-team-all.html', result)
+    html = tp.ParseCertificateTemplate(TEMPLATE_DIR, TEMPLATE_TEAM_COMBI, result)
 
     rank = 1
     for cert  in html.getCertificate():
-        #pdfkit.from_string(cert, OUTPUT_DIR+"teamAll"+str(rank)+".pdf")
-        with open(OUTPUT_TEAM_ALL.format(str(rank)), mode="w", encoding='utf-8') as f:
+        with open(OUTPUT_TEAM_COMBI.format(str(rank)), mode="w", encoding='utf-8') as f:
             f.write(cert)
         rank += 1
         
 
 if __name__ == "__main__":
-    createCertsTeamAll()
+    parser = argparse.ArgumentParser(description = 'クラブ戦用の賞状をhtmlで作成する')
+    parser.add_argument('--position', '-p', nargs='*',
+                        help='作成する種目')
+    parser.add_argument('--team', '-t', action='store_true',
+                        help='種目別団体の表彰状を作成')
+    parser.add_argument('--all', '-a', action='store_true',
+                        help='全種目の表彰状を作成')
+    parser.add_argument('--combined', '-c', action='store_true',
+                        help='総合団体の表彰状を作成')
+    parser.add_argument('--version', '-v', action='version',
+                        version=os.path.basename(__file__) + ' ver.0.1')
+    args = parser.parse_args()
+
+    
+    if args.combined:
+        createCertsTeamCombined()
     
