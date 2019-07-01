dd857dc45bec1eb3b58bc67feae0cc2cdf431b93
s-ariga <seiichi.ariga@gmail.com>
2019-07-01 12:47:10 +0900

    姿勢名リストを変更

--- scripts/Positions.py
+++ scripts/Positions.py
@@ -10,18 +10,25 @@ class Positions(Enum):
     AR60PR = 7
 #    ARMIX = 6
 
-AR = ['AR60',
-      'AR60W',
-      'AR60PR']
+'''
+種目名　
+テンプレート読み込み前に、この中に無い種目名はエラーを返します
+
+** Mix種目は射手数がちがうので別にする
+** ARとSBを分けたのは気分
+'''
+ARM = ['AR60']
+ARW = ['AR60W']
+AR = ['AR60PR']
 
 MIX = ['ARMIX']
 
-SB = ['FR3X40',
-      'FR60PR',
-      'R3X40',
+SBM = ['FR3X40',
+      'FR60PR']
+SBW = ['R3X40',
       'R60PR']
 
-POSITIONS = AR + SB + MIX
+POSITIONS = ARM + ARW + AR + SBM + SBW + MIX
 
 def position_exists(name):
     '''
