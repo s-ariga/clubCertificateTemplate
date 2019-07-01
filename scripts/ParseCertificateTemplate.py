121d9ecf4903f88b005d5ae8f5dbe8db1c53ce5a
s-ariga <seiichi.ariga@gmail.com>
2019-07-01 12:38:13 +0900

    設定ファイルからの読み込みに対応

--- scripts/ParseCertificateTemplate.py
+++ scripts/ParseCertificateTemplate.py
@@ -25,6 +25,14 @@ CERT_RANKS = 6
 
 class ParseCertificateTemplate():
     def __init__(self, path, filename, data):
+        '''
+        初期化でjinja2のインスタンスを作る
+        
+        parameters
+        path : テンプレートの相対パス str
+        filename : テンプレートのファイル名 str
+        data : 成績 DataFrame
+        '''
         self.tmplt_loader = jinja2.FileSystemLoader(searchpath=path)
         self.tmplt_env = jinja2.Environment(loader=self.tmplt_loader)
         self.tmplt_file = filename
@@ -36,21 +44,60 @@ class ParseCertificateTemplate():
         self.year = self.get_reiwa(today.year, gannen = True)
         self.month, self.day = today.month, today.day
 
-    def getCertificate(self):
+    def getCertificate(self, position='', team=False):
         '''
         テンプレートに成績を流し込み、順位順にhtmlのリストに入れて返す
+
+        parameters
+        position : 種目名 デフォルト=団体
+        team : 種目別団体 デフォルト=種目別個人
+
+        returns
+        html : データを流し込んだテンプレート
         '''
+        assert type(position) is str, '種目名指定エラー'
         assert self.tmplt_file is not None, 'Class not properly initialized.'
         template = self.tmplt_env.get_template(self.tmplt_file)
 
         html = []
         for index, item in self.df.iterrows():
-            output = template.render(score = item[SCORE],
-                                     rank = item[RANK],
-                                     team = item[TEAM],
-                                     year = self.year,
-                                     month = self.month,
-                                     day = self.day)
+            if position in posi.AR+posi.SB:
+                if team == True:
+                    output = template.render(position = item[POSI],
+                                             score = item[SCORE],
+                                             rank = item[RANK],
+                                             team = item[TEAM],
+                                             name1 = item[NAME1],
+                                             name2 = item[NAME2],
+                                             name3 = item[NAME3],
+                                             year = self.year,
+                                             month = self.month,
+                                             day = self.day)
+                else:
+                    output = template.render(position = item[POSI],
+                                             score = item[SCORE],
+                                             rank = item[RANK],
+                                             team = item[TEAM],
+                                             name = item[NAME],
+                                             year = self.year,
+                                             month = self.month,
+                                             day = self.day)
+            elif position == posi.MIX:
+                output = template.render(score = item[SCORE],
+                                         rank = item[RANK],
+                                         team = item[TEAM],
+                                         name1 = item[NAME1],
+                                         name2 = item[NAME2],
+                                         year = self.year,
+                                         month = self.month,
+                                         day = self.day)
+            else:
+                output = template.render(score = item[SCORE],
+                                         rank = item[RANK],
+                                         team = item[TEAM],
+                                         year = self.year,
+                                         month = self.month,
+                                         day = self.day)            
             html.append(output)
         return html
 
--- scripts/createCerts.py
+++ scripts/createCerts.py
@@ -2,6 +2,7 @@
 # -*- coding : utf-8 -*-
 
 import os
+import sys
 import datetime
 import argparse
 
@@ -11,39 +12,64 @@ import pandas as pd
 import ParseCertificateTemplate as tp
 import Positions as pos
 
-# テンプレート置き場
-TEMPLATE_DIR = "../template/"
-TEMPLATE_INDIVIDUAL = "certificate-indivi.html"
-TEMPLATE_MIXTEAM = "certificate-mixteam.html"
-TEMPLATE_TEAM_POSI = "certificate-tem-posi.html"
-TEMPLATE_TEAM_COMBI = "certificate-team-combi.html"
-
-# 成績置き場
-RESULT_DIR = "../results/"
-RESULT_INDIVIDUAL = RESULT_DIR + "individual.xlsx"
-RESULT_MIXTEAM = RESULT_DIR + "mixteam.xlsx"
-RESULT_TEAM_POSI = RESULT_DIR + "team_posi.xlsx"
-RESULT_TEAM_COMBI = RESULT_DIR + "team_combi.xlsx"
-
-# 出力先
-OUTPUT_DIR = "../output/"
-OUTPUT_TEAM_COMBI = OUTPUT_DIR+"teamCombined{0}.html"
+from ClubConfig import *
 
 def createCerts():
     pass
 
+def createCertsIndivitual(positions):
+    '''
+    個人種目の表彰状を作成
+    '''
+    result = pd.read_excel(RESULT_INDIVIDUAL, sheet_name=positions)
+    '''
+    with pd.ExcelWriter('test_posi.xlsx') as writer:    
+        for pos in positions:
+            result[pos].to_excel(writer, sheet_name=pos, index=False)
+    '''
+    html = {}
+    cert_html = {}
+    for pos in positions:
+        html[pos] = tp.ParseCertificateTemplate(TEMPLATE_DIR, TEMPLATE_INDIVIDUAL, result[pos])
+        cert_html[pos] = html[pos].getCertificate(position=pos)
+        outputHTML(cert_html[pos], OUTPUT_INDIVIDUAL, pos)
+
+def createCertsTeam(positions):
+    '''
+    種目別団体の表彰状を作成
+    '''
+    pass
+
+def createCertsMixTeam():
+    '''
+    ミックスチームの表彰状を作成
+    '''
+    result = pd.read_excel(RESULT_MIXTEAM)
+    html = tp.ParseCertificateTemplate(TEMPLATE_DIR, TEMPLATE_MIXTEAM, result)
+    cert_html = html.getCertificate(position='ARMIX')
+    outputHTML(cert_html, OUTPUT_MIXTEAM)
+
 def createCertsTeamCombined():
-
+    '''
+    総合団体の賞状を作成する
+    '''
     result = pd.read_excel(RESULT_TEAM_COMBI)
-    
     html = tp.ParseCertificateTemplate(TEMPLATE_DIR, TEMPLATE_TEAM_COMBI, result)
+    cert_html = html.getCertificate()
+    outputHTML(cert_html, OUTPUT_TEAM_COMBI)
 
+
+def outputHTML(html, filename, pos = ''):
+    '''
+    HTMLをファイル出力
+    '''
+    assert type(pos) is str, '種目名指定エラー' 
     rank = 1
-    for cert  in html.getCertificate():
-        with open(OUTPUT_TEAM_COMBI.format(str(rank)), mode="w", encoding='utf-8') as f:
+    for cert  in html:
+        assert rank < 7, '順位が7位以上あります'
+        with open(filename.format(pos, str(rank)), mode='w', encoding='utf-8') as f:
             f.write(cert)
         rank += 1
-        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'クラブ戦用の賞状をhtmlで作成する')
@@ -51,14 +77,34 @@ if __name__ == "__main__":
                         help='作成する種目')
     parser.add_argument('--team', '-t', action='store_true',
                         help='種目別団体の表彰状を作成')
-    parser.add_argument('--all', '-a', action='store_true',
-                        help='全種目の表彰状を作成')
+    parser.add_argument('--mixteam', '-m', action='store_true',
+                        help='ミックスチームの表彰状を作成')
+    parser.add_argument('--all_positions', '-a', action='store_true',
+                        help='全種目の表彰状を作成.総合団体ミックスチーム以外')
     parser.add_argument('--combined', '-c', action='store_true',
                         help='総合団体の表彰状を作成')
     parser.add_argument('--version', '-v', action='version',
                         version=os.path.basename(__file__) + ' ver.0.1')
     args = parser.parse_args()
 
+    if args.position:
+        for p in args.position:
+            if not pos.position_exists(p):
+                print('不正な種目名です. 種目名は以下のどれか')
+                print(pos.POSITIONS)
+                assert False, '種目名エラー'
+                sys.exit('Position name Error.')
+        createCertsIndivitual(args.position)
+        if args.team:
+            createCertsTeam(args.position)
+
+    if args.mixteam:
+        createCertsMixTeam()
+        
+    if args.all_positions:
+        createCertsIndividual(pos.POSITIONS)
+        if args.team:
+            createCertsTeam(pos.POSITIONS)
     
     if args.combined:
         createCertsTeamCombined()
