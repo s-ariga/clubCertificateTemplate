69fa34ebaaf30d3cd6f5e3e8f7a3a23a944806be
s-ariga <seiichi.ariga@gmail.com>
2019-07-19 14:56:48 +0900

    テスト印刷スクリプト

---
+++ template/testPrint.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+import pdfkit
+import sys
+import os
+
+html = sys.argv[1]
+
+html_base = os.path.splitext(html)[0]
+
+pdf_out = html_base + '.pdf'
+
+config = pdfkit.configuration(wkhtmltopdf = 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+
+pdfkit.from_file(html, pdf_out, configuration = config)
